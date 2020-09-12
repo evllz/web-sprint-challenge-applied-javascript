417ec08d8eb3c365994d38e5ea2b6c4d44b64c3d
evllz <erickvillegsaecheverria@gmail.com>
2020-09-11 21:09:16 -0700

    MVP + Unfinished STRETCH

--- components/Carousel.js
+++ components/Carousel.js
@@ -22,3 +22,60 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function carousel(){
+  const carousel_container = document.createElement('div');
+  carousel_container.classList.add('carousel');
+    const left_button = document.createElement('div');
+    left_button.classList.add('left-button');
+    carousel_container.appendChild(left_button);
+
+    left_button.addEventListener('click',() => {
+      const value = document.querySelector('.carousel-img');
+      if(value.src === 'https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg'){
+        img.src = 'https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg';
+      }
+      else if (value.src === 'https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg'){
+        img.src = 'https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg'
+      }
+      else if (value.src === 'https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg'){
+        img.src = 'https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg'
+      }
+      else if (value.src === 'https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg'){
+        img.src = 'https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg'
+      }
+    })
+
+    const right_button = document.createElement('div');
+    right_button.classList.add('right-button');
+    carousel_container.appendChild(right_button);
+
+    right_button.addEventListener('click',() => {
+      const value = document.querySelector('.carousel-img');
+      value.style.animation = 'slide 0.5s backwards';
+      if(value.src === 'https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg'){
+        img.src = 'https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg';
+      }
+      else if (value.src === 'https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg'){
+        img.src = 'https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg'
+      }
+      else if (value.src === 'https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg'){
+        img.src = 'https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg'
+      }
+      else if (value.src === 'https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg'){
+        img.src = 'https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg'
+      }
+    })
+
+    const img = document.createElement('img');
+    img.classList.add('carousel-img');
+    img.src = 'https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg';
+    carousel_container.appendChild(img);
+
+
+  return carousel_container;
+}
+
+const container = document.querySelector('.carousel-container');
+const newCarousel = carousel();
+container.appendChild(newCarousel)
